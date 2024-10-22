82378696b8d96b821847b2aa610fc46725c29297
GaziYucel <GaziYucel@users.noreply.github.com>
2024-10-22 16:34:24 +0200

    Migrate affiliations to multiple affiliations

--- classes/migration/upgrade/v3_4_0/I7135_CreateNewRorRegistryCacheTables.php
+++ classes/migration/upgrade/v3_4_0/I7135_CreateNewRorRegistryCacheTables.php
@@ -13,7 +13,9 @@
 
 namespace PKP\migration\upgrade\v3_4_0;
 
+use Illuminate\Database\Query\JoinClause;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema as Schema;
 use PKP\install\DowngradeNotSupportedException;
 use PKP\migration\Migration;
@@ -55,6 +57,10 @@ class I7135_CreateNewRorRegistryCacheTables extends Migration
         // update the tables with latest data set dump from Ror.org
         $updateRorRegistryDataset = new UpdateRorRegistryDataset();
         $updateRorRegistryDataset->execute();
+
+        $this->migrateRorAffiliations();
+
+        $this->migrateNonRorAffiliations();
     }
 
     /**
@@ -65,5 +71,89 @@ class I7135_CreateNewRorRegistryCacheTables extends Migration
     public function down(): void
     {
         throw new DowngradeNotSupportedException();
+    }
+
+    /**
+     * Migrates affiliations in author_settings.
+     * If the name is exact as in rors and ror_settings tables,
+     * ROR data is migrated.
+     *
+     * @return void
+     */
+    private function migrateRorAffiliations(): void
+    {
+        // migrate author.affiliation field
+        // add ROR if affiliation name match
+        // select r.ror_id, a_s.author_id, r.ror, r_s.setting_value, r_s.locale
+        // from rors as r
+        // join ror_settings as r_s on r.ror_id = r_s.ror_id
+        // join `author_settings` as a_s on r_s.setting_value = a_s.setting_value
+        // where r_s.setting_name = 'name' and a_s.setting_name = 'affiliation' and r_s.locale = a_s.locale
+        $rows = DB::table('rors as r')
+            ->join('ror_settings as r_s',
+                function (JoinClause $join) {
+                    $join
+                        ->on('r.ror_id', '=', 'r_s.ror_id')
+                        ->where('r_s.setting_name', '=', 'name');
+                }
+            )
+            ->join('author_settings as a_s',
+                function (JoinClause $join) {
+                    $join
+                        ->on('r_s.setting_value', '=', 'a_s.setting_value')
+                        ->on('r_s.locale', '=', 'a_s.locale')
+                        ->where('a_s.setting_name', '=', 'affiliation');
+                }
+            )
+            ->select(['r.ror_id', 'a_s.author_id', 'r.ror', 'r_s.setting_value', 'r_s.locale'])
+            ->get();
+
+        foreach ($rows as $row) {
+            //fixme: multiple-author-affiliations
+            // insert into tables author_affiliations and author_affiliation_settings
+            error_log(
+                '[author_id: ' . $row['author_id'] . ']' .
+                '[ror_id: ' . $row['ror_id'] . ']' .
+                json_encode($row, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE)
+            );
+        }
+    }
+
+    /**
+     * Migrates affiliations which have not migrated yet.
+     *
+     * @return void
+     */
+    private function migrateNonRorAffiliations(): void
+    {
+        // add as custom affiliation if ROR does not match
+        // select a.author_id, a_s.locale, a_s.setting_value
+        // from authors as a
+        // join author_settings as a_s on a.author_id = a_s.author_id and a_s.setting_name = 'affiliation'
+        // left join author_affiliations as aa on a.author_id = aa.author_id
+        // where aa.author_id is null
+        $rows = DB::table('authors as a')
+            ->join('author_settings as a_s',
+                function (JoinClause $join) {
+                    $join
+                        ->on('a.author_id', '=', 'a_s.author_id')
+                        ->where('a_s.setting_name', '=', 'affiliation');
+                }
+            )
+            ->leftJoin('author_affiliations as aa','a.author_id', '=', 'aa.author_id')
+            ->where('aa.author_id', '=', null)
+            ->select(['a.author_id', 'a_s.locale', 'a_s.setting_value'])
+            ->get();
+
+        foreach ($rows as $row) {
+            //fixme: multiple-author-affiliations
+            // insert into tables author_affiliations and author_affiliation_settings
+            error_log(
+                '[author_id: ' . $row['author_id'] . ']' .
+                '[locale: ' . $row['locale'] . ']' .
+                '[setting_value: ' . $row['setting_value'] . ']' .
+                json_encode($row, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE)
+            );
+        }
     }
 }
